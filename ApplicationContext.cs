5cca18e0250df53e19c822eb737d9ad21f3d2804
Mathieu Savaria <mathieu.savaria@apcurium.com>
2018-11-26 14:53:48 -0500

    Initial commit

---
+++ ApplicationContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MigrationBugRepro.Models;
+
+namespace MigrationBugRepro
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Account>(b =>
+            {
+                b.HasKey(x => x.AccountId);
+                b.Property(e => e.AccountId).HasMaxLength(36).IsRequired();
+
+                // Multiple Owned Types
+                b.OwnsOne(e => e.MainAddress, a =>
+                {
+                    a.Property(x => x.AddressLine1).HasMaxLength(256);
+                    a.Property(x => x.City).HasMaxLength(256);
+                });
+                b.OwnsOne(e => e.SecondaryAddress, a =>
+                {
+                    a.Property(x => x.AddressLine1).HasMaxLength(256);
+                    a.Property(x => x.City).HasMaxLength(256);
+                });
+            });
+        }
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MigrationBugRepro.Models
+{
+    public class Account
+    {
+        public string AccountId { get; set; }
+
+        public Address MainAddress { get; set; }
+        public Address SecondaryAddress { get; set; }
+    }
+}
